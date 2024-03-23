3438490292ed179e890c4b24169dc2c542cad950
relliv <epsilonelittr@gmail.com>
2024-03-23 06:18:39 +0300

    Fix typo

--- tools/copyAssets.js
+++ tools/copyAssets.js
@@ -4,6 +4,6 @@ copy("./README.md", "./dist/ngx-react/README.md", (err) => {
   if (err) return console.error(err)
 })
 
-copy("./license", "./dist/editor/LICENSE", (err) => {
+copy("./license", "./dist/ngx-react/LICENSE", (err) => {
   if (err) return console.error(err)
 })
